d6c7f532ba973b069e011912f0e7277f3bf80979
ekusuy <ekusuy.omi@gmail.com>
2024-02-12 15:23:28 +0900

    first commit

---
+++ html/wp-content/themes/pacific/sidebar.php
@@ -0,0 +1,9 @@
+<section id="sidebar">
+    <div id="primary" class="widget-area">
+        <?php dynamic_sidebar('primary-widget-area'); ?>
+    </div><!-- #primary end -->
+    <div id="secondary" class="widget-area">
+        facebook
+    </div>
+
+</section><!-- #sidebar end -->
